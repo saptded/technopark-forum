eef972f4b5bc30090ecd13e07175bd7b6f51439e
saptded <a.kosenko@vk.team>
2022-01-21 08:16:25 +0300

    Another try

--- db/db.sql
+++ db/db.sql
@@ -26,7 +26,7 @@ CREATE TABLE threads
     forum      CITEXT COLLATE "C",
     message    TEXT NOT NULL,
     votes      INTEGER DEFAULT 0,
-    slug       CITEXT,
+    slug       CITEXT  DEFAULT NULL,
     created_at TIMESTAMP WITH TIME ZONE
 );
 
